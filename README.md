733a8ed336042c23a095eb67b8cd11c71b78e457
inferno0230 <mail@inferno0230.in>
2025-07-17 20:58:11 +0530

    [SQUASH] touch: Move back to HIDL

--- README.md
+++ README.md
@@ -8,10 +8,8 @@
 | OPLUS_LINEAGE_LIVEDISPLAY_HAL | ENABLE_DM | Enable DisplayModes feature | false |
 | OPLUS_LINEAGE_LIVEDISPLAY_HAL | ENABLE_PA | Enable PictureAdjustment feature | true |
 | OPLUS_LINEAGE_LIVEDISPLAY_HAL | ENABLE_SE | Enable SunlightEnhancement feature | true |
-| OPLUS_LINEAGE_TOUCH_HAL | ENABLE_HTPR | Enable HighTouchPollingRate feature | true |
-| OPLUS_LINEAGE_TOUCH_HAL | ENABLE_TG | Enable TouchscreenGesture feature | true |
 | OPLUS_LINEAGE_TOUCH_HAL | INCLUDE_DIR | Device specific include dir path | |
-| OPLUS_LINEAGE_TOUCH_HAL | USE_OPLUSTOUCH | Use and interface with stock OplusTouch | false |
+| OPLUS_LINEAGE_TOUCH_HAL | USE_OPLUSTOUCH | Enable OplusTouch feature | false |
 | OPLUS_LINEAGE_VIBRATOR_HAL | INCLUDE_DIR | Device specific include dir path | |
 | OPLUS_LINEAGE_VIBRATOR_HAL | USE_EFFECT_STREAM | Enable effect stream feature | false |
 | QTI_GPT_UTILS | USE_BSG_FRAMEWORK | Enable BSG framework feature | true |
--- hidl/touch/TouchscreenGesture.h
+++ hidl/touch/TouchscreenGesture.h
@@ -1,31 +1,49 @@
 /*
- * SPDX-FileCopyrightText: 2025 The LineageOS Project
- * SPDX-License-Identifier: Apache-2.0
+ * Copyright (C) 2022 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 #pragma once
 
-#include <aidl/vendor/lineage/touch/BnTouchscreenGesture.h>
+#ifdef USE_OPLUSTOUCH
 #include <aidl/vendor/oplus/hardware/touch/IOplusTouch.h>
+#endif
+#include <hidl/MQDescriptor.h>
+#include <hidl/Status.h>
+#include <vendor/lineage/touch/1.0/ITouchscreenGesture.h>
 #include <map>
 
-namespace aidl {
 namespace vendor {
 namespace lineage {
 namespace touch {
+namespace V1_0 {
+namespace implementation {
 
-using aidl::vendor::oplus::hardware::touch::IOplusTouch;
+#ifdef USE_OPLUSTOUCH
+using ::aidl::vendor::oplus::hardware::touch::IOplusTouch;
+#endif
+using ::android::hardware::Return;
+using ::android::hardware::Void;
+using ::vendor::lineage::touch::V1_0::Gesture;
 
-class TouchscreenGesture : public BnTouchscreenGesture {
+class TouchscreenGesture : public ITouchscreenGesture {
   public:
-    explicit TouchscreenGesture(std::shared_ptr<IOplusTouch> oplusTouch);
-
-    ndk::ScopedAStatus getSupportedGestures(std::vector<Gesture>* _aidl_return) override;
-    ndk::ScopedAStatus setGestureEnabled(const Gesture& gesture, bool enabled) override;
+    // Methods from ::vendor::lineage::touch::V1_0::ITouchscreenGesture follow.
+    Return<void> getSupportedGestures(getSupportedGestures_cb resultCb) override;
+    Return<bool> setGestureEnabled(const Gesture& gesture, bool enabled) override;
 
   private:
-    std::shared_ptr<IOplusTouch> mOplusTouch;
-
     // See: drivers/input/touchscreen/oplus_touchscreen_v2/touchpanel_common.h
     static constexpr int kGestureStartKey = 246;
     enum {
@@ -77,9 +95,13 @@ class TouchscreenGesture : public BnTouchscreenGesture {
         return ((1 << head) | ... | (1 << tail));
     }
     static const int kSupportedGestures;
+#ifdef USE_OPLUSTOUCH
+    std::shared_ptr<IOplusTouch> mTouchService;
+#endif
 };
 
+}  // namespace implementation
+}  // namespace V1_0
 }  // namespace touch
 }  // namespace lineage
 }  // namespace vendor
-}  // namespace aidl
